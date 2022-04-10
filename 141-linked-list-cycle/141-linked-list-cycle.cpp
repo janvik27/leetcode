10c0fbe319ea3241d9aee92e57cec4a012f7656c
Janvi Kundnani <65007679+janvik27@users.noreply.github.com>
2022-04-10 17:35:27 +0530

    Time: 8 ms (94.49%), Space: 8.1 MB (15.14%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -9,6 +9,8 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
+        // approach 1: using map or set
+        /*
         // ht= hash table
         unordered_set<ListNode*> ht;
         
@@ -23,5 +25,33 @@ public:
                 return true;
         }
         return false;
+        */
+        
+        // APPROACH 2 : FLOYD'S DETECTION ALGO : TWO POINTER APPROACH
+        if(head==NULL)
+            return false;
+        
+        if(head->next==NULL)
+            return false;
+        
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while(fast!=NULL && slow!=NULL)
+        {
+            fast = fast->next;
+            if(fast!=NULL)
+                fast = fast->next;
+            
+            slow= slow->next;
+            
+            if(slow==fast)
+                return true;
+        }
+        
+        return false;
+        
+        
     }
 };
