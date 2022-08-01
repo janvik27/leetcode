80de097ff328a110a450d94370b1dad61e7d94b9
Janvi Kundnani <65007679+janvik27@users.noreply.github.com>
2022-08-01 13:59:49 +0530

    Time: 4 ms (28.94%), Space: 5.9 MB (75.04%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,19 +1,33 @@
 class Solution {
 public:
-    int countPaths(int m,int n,int i,int j,vector<vector<int>> &dp)
-    {
-        if(i==m-1 && j==n-1)
-            return 1;
-        if(i>=m || j>=n)
-            return 0;
+//     int countPaths(int m,int n,int i,int j,vector<vector<int>> &dp)
+//     {
+//         if(i==m-1 && j==n-1)
+//             return 1;
+//         if(i>=m || j>=n)
+//             return 0;
         
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        else
-            return dp[i][j] = countPaths(m,n,i+1,j,dp) + countPaths(m,n,i,j+1,dp);
-    }
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return countPaths(m,n,0,0,dp);
+//         if(dp[i][j]!=-1)
+//             return dp[i][j];
+//         else
+//             return dp[i][j] = countPaths(m,n,i+1,j,dp) + countPaths(m,n,i,j+1,dp);
+//     }
+//     int uniquePaths(int m, int n) {
+//         vector<vector<int>> dp(m,vector<int>(n,-1));
+//         return countPaths(m,n,0,0,dp);
+//     }
+    
+    int uniquePaths(int m,int n)
+    {
+        int N = m+n-2;
+        int R= m-1;
+        double res=1;
+        
+        for(int i=1;i<=R;i++)
+        {
+            res= res * (N-R+i)/i;
+        }
+        return (int)res;
+        
     }
 };
